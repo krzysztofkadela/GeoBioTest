edea62eefff3fbe920915cc4d08be6d330d81d6c
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-06-15 19:27:57 +0000

    Development changes in and out...Progrs main paga ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,92 +14,90 @@
 //set questions and 4 answers for each, only one correct.
 
 
-const questionsBio = [
-    {
+const questionsBio = [{
         question: "What animal is the fastest in the world?",
         answers: {
-            1:"answer1",
-            2:"answer2",
-            3:"answer3",
-            4:"answer3"
+            1: "answer1",
+            2: "answer2",
+            3: "answer3",
+            4: "answer3"
         },
         correctAnswer: "3"
     },
     {
         question: "Question2",
         answers: {
-            1:"answer1",
-            2:"answer2",
-            3:"answer3",
-            4:"answer3"
+            1: "answer1",
+            2: "answer2",
+            3: "answer3",
+            4: "answer3"
         },
         correctAnswer: "3"
     },
     {
         question: "Question3",
         answers: {
-            1:"answer1",
-            2:"answer2",
-            3:"answer3",
-            4:"answer3"
+            1: "answer1",
+            2: "answer2",
+            3: "answer3",
+            4: "answer3"
         },
         correctAnswer: "3"
     },
     {
         question: "Question4",
         answers: {
-            1:"answer1",
-            2:"answer2",
-            3:"answer3",
-            4:"answer3"
+            1: "answer1",
+            2: "answer2",
+            3: "answer3",
+            4: "answer3"
         },
         correctAnswer: "3"
     }
-    
+
 ];
 
-const questionsGeo = [
-    {
+const questionsGeo = [{
         question: "Question1",
         answers: {
-            1:"answer1",
-            2:"answer2",
-            3:"answer3",
-            4:"answer3"
+            1: "answer1",
+            2: "answer2",
+            3: "answer3",
+            4: "answer3"
         },
         correctAnswer: "3"
     },
     {
         question: "Question1",
         answers: {
-            1:"answer1",
-            2:"answer2",
-            3:"answer3",
-            4:"answer3"
+            1: "answer1",
+            2: "answer2",
+            3: "answer3",
+            4: "answer3"
         },
         correctAnswer: "3"
     },
     {
         question: "Question1",
         answers: {
-            1:"answer1",
-            2:"answer2",
-            3:"answer3",
-            4:"answer3"
+            1: "answer1",
+            2: "answer2",
+            3: "answer3",
+            4: "answer3"
         },
         correctAnswer: "3"
     },
     {
         question: "Question1",
         answers: {
-            1:"answer1",
-            2:"answer2",
-            3:"answer3",
-            4:"answer3"
+            1: "answer1",
+            2: "answer2",
+            3: "answer3",
+            4: "answer3"
         },
         correctAnswer: "3"
     }
-    
+
 ];
 
 
@@ -108,12 +106,15 @@ const questionsGeo = [
 
 // Wait for DOM to finish loading , get buttons and add events listeners to them.
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("id") === "submit") {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("id") === "start") {
+                alert(`You clicked start`);
+                document.getElementById("question_area").classList.remove("hidden");
+            } else if (this.getAttribute("id") === "submit") {
                 clicked();
                 alert("You clicked Submit!");
             } else {
@@ -128,7 +129,7 @@ document.addEventListener("DOMContentLoaded", function() {
     //startQuize("geografi");
 
 });
-    
+
 
 
 
@@ -138,93 +139,101 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Start quiz function , takes a parameter to select the appropriate quiz.
 
-function startQuize(quizType){
-    let correctAnswer = document.getElementById("correct");//to be check.
+function startQuize(quizType) {
+    let correctAnswer = document.getElementById("correct"); //to be check.
 
     //if geography
-if(quizType === "game_geography"){
-    document.getElementById("game_geography").classList.add("hidden");
-    document.getElementById("question_area").style.backgroundColor="lightblue"; 
-    loadGeoQuestions();
+    if (quizType === "game_geography") {
+        document.getElementById("game_geography").classList.add("hidden");
+        document.getElementById("question_area").style.backgroundColor = "lightblue";
+        loadGeoQuestions();
 
-}else if(quizType === "game_biology"){
-    let domi = new Array();
-    domi = rendomNumber();
-    document.getElementById("game_biology").classList.add("hidden");
-    document.getElementById("question_area").style.backgroundColor="lightgray";
-    loadBioQuestions(domi);
-}else{
-    alert(`something went wrong, please try again`);
-    resetQuize();
-}
+    } else if (quizType === "game_biology") {
+        let domi = new Array();
+        domi = rendomNumber();
+        document.getElementById("game_biology").classList.add("hidden");
+        document.getElementById("question_area").style.backgroundColor = "lightgray";
+        loadBioQuestions(domi);
+    } else {
+        alert(`something went wrong, please try again`);
+        resetQuize();
+    }
 
 }
 
 // 2 functions to display(load) questions for diferent quiz type.
 
-function loadGeoQuestions(){
-    
+function loadGeoQuestions() {
+
 
 
 }
 
-function loadBioQuestions(domi){
+function loadBioQuestions(domi) {
 
- document.getElementById("question").innerHTML=questionsBio[2].question;
+    document.getElementById("question").innerHTML = questionsBio[2].question;
 
-    document.getElementById("answer_1").innerHTML=(item[0]);
-    document.getElementById("answer_2").innerHTML=(item[1]);
-    document.getElementById("answer_3").innerHTML=(item[2]);
-    document.getElementById("answer_4").innerHTML=(item[3])
+    document.getElementById("answer_1").innerHTML = (item[0]);
+    document.getElementById("answer_2").innerHTML = (item[1]);
+    document.getElementById("answer_3").innerHTML = (item[2]);
+    document.getElementById("answer_4").innerHTML = (item[3])
 }
 
 //Check ansver function, to check answer and increase conter for correct or inncorect.
 
-function submitAnswer(){
+function submitAnswer() {
 
 
 }
 
-function resetQuize(){};
+function resetQuize() {};
 
 //rendom number function for  placing questions in random order
 
-function rendomNumber(){
+function rendomNumber() {
     //number of questions to be drawn.
     let questionNumber = 4;
     //number of questions available.
     let questionFrom = 4;
-     
-    //new array to hold qustions numbers.
-    let numbers= new Array();
 
-    for (let i=0;i<questionNumber.length;i++) {
-      let rand = Math.round(Math.random()*(questionFrom-1))+1;
-      let exist=false;
-      //if number exist in array numbers , get new number.
-      for (let j=0;j<numbers.length;j++) if (numbers[j]===rand) {exist=true;}
-      if (rand) {i--;} else {numbers[i]=rand;}
+    //new array to hold qustions numbers.
+    let numbers = new Array();
+
+    for (let i = 0; i < questionNumber.length; i++) {
+        let rand = Math.round(Math.random() * (questionFrom - 1)) + 1;
+        let exist = false;
+        //if number exist in array numbers , get new number.
+        for (let j = 0; j < numbers.length; j++)
+            if (numbers[j] === rand) {
+                exist = true;
+            }
+        if (rand) {
+            i--;
+        } else {
+            numbers[i] = rand;
+        }
     }
-    return numbers;  
+    return numbers;
 }
-function clicked(){
-submitBtn.addEventListener('click', function() {
-    const answer = getSelected()
-    if(answer) {
-       if(answer === quizData[currentQuiz].correct) {
-           score++
-       }
-       currentQuiz++
-       if(currentQuiz < quizData.length) {
-           loadQuiz()
-       } else {
-           quiz.innerHTML = `
+
+function clicked() {
+    submitBtn.addEventListener('click', function () {
+        const answer = getSelected()
+        if (answer) {
+            if (answer === quizData[currentQuiz].correct) {
+                score++
+            }
+            currentQuiz++
+            if (currentQuiz < quizData.length) {
+                loadQuiz()
+            } else {
+                quiz.innerHTML = `
            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
            <button onclick="location.reload()">Reload</button>
            `
-       }
-    }
+            }
+        }
 
-})
+    })
 
 }
