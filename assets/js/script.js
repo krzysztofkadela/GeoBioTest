daf387e66d1b84f0a353f6399532dda302ec5f0e
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-06-25 17:36:52 +0000

    Code: updtate cod to testrandom numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,239 +1,255 @@
-// setings some variable.
-//const question = document.querySelector('#question');
-
-//varible with all answers for question.
-
-//const answer = Arrayfrom(document.querySelectorAll(".p_answer"));
-
-
-
-
-
-
-
-//set questions and 4 answers for each, only one correct.
-
+//questions and 4 answers with one correct
 
 const questionsBio = [{
-        question: "What animal is the fastest in the world?",
-        answers: {
-            1: "answer1",
-            2: "answer2",
-            3: "answer3",
-            4: "answer3"
-        },
-        correctAnswer: "3"
-    },
-    {
-        question: "Question2",
-        answers: {
-            1: "answer1",
-            2: "answer2",
-            3: "answer3",
-            4: "answer3"
-        },
-        correctAnswer: "3"
-    },
-    {
-        question: "Question3",
-        answers: {
-            1: "answer1",
-            2: "answer2",
-            3: "answer3",
-            4: "answer3"
-        },
-        correctAnswer: "3"
-    },
-    {
-        question: "Question4",
-        answers: {
-            1: "answer1",
-            2: "answer2",
-            3: "answer3",
-            4: "answer3"
-        },
-        correctAnswer: "3"
-    }
+    question: "What land animal is the fastest in the world?",
+    a: "Giraffe",
+    b: "wolf",
+    c: "Cheetah",
+    d: "Gazelles",
+    correctAnswer: "c",
+},
+{
+    question: "What kind of tree is the tallest in the world?",
+    a: "Sequoia",
+    b: "Oak",
+    c: "Pine",
+    d: "Bamboo",
+    correctAnswer: "a",
+},
+{
+    question: "Which of the following animals is not a mammal?",
+    a: "Elephant",
+    b: "Tiger",
+    c: "Dolphin",
+    d: "Turtle",
+    correctAnswer: "d",
+},
+{
+    question: "What animal is the largest on earth?",
+    a: "Bison",
+    b: "Giraffe",
+    c: "Elephant",
+    d: "Blue whale",
+    correctAnswer: "d",
+}
 
 ];
 
 const questionsGeo = [{
-        question: "Question1",
-        answers: {
-            1: "answer1",
-            2: "answer2",
-            3: "answer3",
-            4: "answer3"
-        },
-        correctAnswer: "3"
-    },
-    {
-        question: "Question1",
-        answers: {
-            1: "answer1",
-            2: "answer2",
-            3: "answer3",
-            4: "answer3"
-        },
-        correctAnswer: "3"
-    },
-    {
-        question: "Question1",
-        answers: {
-            1: "answer1",
-            2: "answer2",
-            3: "answer3",
-            4: "answer3"
-        },
-        correctAnswer: "3"
-    },
-    {
-        question: "Question1",
-        answers: {
-            1: "answer1",
-            2: "answer2",
-            3: "answer3",
-            4: "answer3"
-        },
-        correctAnswer: "3"
-    }
+    question: "What mountain is the highest in the world?",
+    a: "Sagarmatha",
+    b: "Lhotse",
+    c: "K2",
+    d: "Manaslu",
+    correctAnswer: "c",
+},
+{
+    question: "Which of the following rivers is the longest?",
+    a: "Brahmaputra",
+    b: "Mississipi",
+    c: "Missouri",
+    d: "Ganges",
+    correctAnswer: "c",
+},
+{
+    question: "How many continents are there on earth?",
+    a: "5",
+    b: "8",
+    c: "3",
+    d: "7",
+    correctAnswer: "d",
+},
+{
+    question: "Which city is the capital of the Republic of Ireland?",
+    a: "Cork",
+    b: "Belfast",
+    c: "Dublin",
+    d: "Golway",
+    correctAnswer: "c",
+}
 
 ];
-
-
-
-
-
-// Wait for DOM to finish loading , get buttons and add events listeners to them.
+//decaray player name varible empty string.
+let playerName = '';
 
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
+let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("id") === "start") {
-                alert(`You clicked start`);
-                document.getElementById("question_area").classList.remove("hidden");
-            } else if (this.getAttribute("id") === "submit") {
-                clicked();
-                alert("You clicked Submit!");
-            } else {
-                let quizType = this.getAttribute("id");
-                startQuize(quizType);
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        if (this.getAttribute("id") === "start") {
+            document.getElementById("welcome").classList.add("invisible");
+            document.getElementById("select_game").classList.remove("invisible");
+            const userName = document.getElementById("myInput").value;
+            playerName=userName;
+        } else if (this.getAttribute("id") === "geo_next_submit") {
 
-                alert(`You clicked ${quizType}`);
+            const answer = submitAnswer()
+            if (answer) {
+                if (answer === questionsGeo[currentQuiz].correctAnswer) {
+                    score++
+                }
+                currentQuiz++ //ned to add random number.
+
+                if (currentQuiz < questionsGeo.length) {
+                    loadGeoQuestions()
+                } else {
+                    //display score function
+                    displayScore(score, playerName);
+                }
             }
-        });
-    }
+        } else if (this.getAttribute("id") === "bio_next_submit") {
+            const answer = submitAnswer()
+            if (answer) {
+                if (answer === questionsBio[currentQuiz].correctAnswer) {
+                    score++
+                }
+                currentQuiz++ //need to add random number.
 
-    //startQuize("geografi");
+                if (currentQuiz < questionsBio.length) {
+                    loadBioQuestions()
+                } else {
+                    displayScore(score, playerName);
+                }
+
+            }
+        } else if(this.getAttribute("id") === "restart"){
+            restartGame();
+
+        }else{
+            let quizType = this.getAttribute("id");
+            startQuize(quizType);
+
+        }
+    });
+}
+
 
 });
+//global variables
+
+let score = 0;
+const nextQuestion = document.getElementById("next_submit");
+// to point quiz container.
+const quize = document.getElementById("question_area");
+// select all elements with class answer
+const quizAnswers = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+
+//Setting up variables for each answer.
+
+const answer1 = document.getElementById("answerText1");
+const answer2 = document.getElementById("answerText2");
+const answer3 = document.getElementById("answerText3");
+const answer4 = document.getElementById("answerText4");
+
+// Call the function to get an array with 4 numbers in random positions
+let result = setQuestionsInRandomPositions();
 
 
+let currentQuiz = 0; //need to add rendom numbers 
+//number of questions to be drawn.
+let questionNumber = 4;
+//number of questions available.
+let questionFrom = 4;
 
 
-
-
-
-
-// Start quiz function , takes a parameter to select the appropriate quiz.
 
 function startQuize(quizType) {
-    let correctAnswer = document.getElementById("correct"); //to be check.
 
-    //if geography
-    if (quizType === "game_geography") {
-        document.getElementById("game_geography").classList.add("hidden");
-        document.getElementById("question_area").style.backgroundColor = "lightblue";
-        loadGeoQuestions();
+//if geography
+if (quizType === "game_geography") {
+    document.getElementById("select_game").classList.add("invisible");
+    document.getElementById("question_area").classList.remove("invisible");
+    document.getElementById("bio_next_submit").classList.add("invisible");
+    loadGeoQuestions();
 
-    } else if (quizType === "game_biology") {
-        let domi = new Array();
-        domi = rendomNumber();
-        document.getElementById("game_biology").classList.add("hidden");
-        document.getElementById("question_area").style.backgroundColor = "lightgray";
-        loadBioQuestions(domi);
-    } else {
-        alert(`something went wrong, please try again`);
-        resetQuize();
-    }
+} else if (quizType === "game_biology") {
+    document.getElementById("select_game").classList.add("invisible");
+    document.getElementById("question_area").classList.remove("invisible");
+    document.getElementById("geo_next_submit").classList.add("invisible");
+    loadBioQuestions();
 
 }
 
-// 2 functions to display(load) questions for diferent quiz type.
+}
+
+//Function to set numbers in random position
+function setQuestionsInRandomPositions() {
+let numbers = [1, 2, 3, 4]; // Number of questions
+let shuffledNumbers = numbers.sort(() => Math.random() - 0.5);
+return shuffledNumbers;
+}
+
+
 
 function loadGeoQuestions() {
+deselectAnswers();
 
+const currentQuizData = questionsGeo[currentQuiz]
 
+questionElement.innerText = currentQuizData.question
+
+answer1.innerText = currentQuizData.a
+answer2.innerText = currentQuizData.b
+answer3.innerText = currentQuizData.c
+answer4.innerText = currentQuizData.d
 
 }
 
-function loadBioQuestions(domi) {
+function loadBioQuestions() {
 
-    document.getElementById("question").innerHTML = questionsBio[2].question;
+deselectAnswers();
 
-    document.getElementById("answer_1").innerHTML = (item[0]);
-    document.getElementById("answer_2").innerHTML = (item[1]);
-    document.getElementById("answer_3").innerHTML = (item[2]);
-    document.getElementById("answer_4").innerHTML = (item[3])
+const currentQuizData = questionsBio[currentQuiz]
+
+questionElement.innerText = currentQuizData.question
+
+answer1.innerText = currentQuizData.a
+answer2.innerText = currentQuizData.b
+answer3.innerText = currentQuizData.c
+answer4.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+quizAnswers.forEach(quizAnswers => quizAnswers.checked = false)
 }
 
 //Check ansver function, to check answer and increase conter for correct or inncorect.
 
 function submitAnswer() {
-
-
-}
-
-function resetQuize() {};
-
-//rendom number function for  placing questions in random order
-
-function rendomNumber() {
-    //number of questions to be drawn.
-    let questionNumber = 4;
-    //number of questions available.
-    let questionFrom = 4;
-
-    //new array to hold qustions numbers.
-    let numbers = new Array();
-
-    for (let i = 0; i < questionNumber.length; i++) {
-        let rand = Math.round(Math.random() * (questionFrom - 1)) + 1;
-        let exist = false;
-        //if number exist in array numbers , get new number.
-        for (let j = 0; j < numbers.length; j++)
-            if (numbers[j] === rand) {
-                exist = true;
-            }
-        if (rand) {
-            i--;
-        } else {
-            numbers[i] = rand;
-        }
+let answer
+quizAnswers.forEach(quizAnswers => {
+    if (quizAnswers.checked) {
+        answer = quizAnswers.id
     }
-    return numbers;
+})
+return answer
 }
 
-function clicked() {
-    submitBtn.addEventListener('click', function () {
-        const answer = getSelected()
-        if (answer) {
-            if (answer === quizData[currentQuiz].correct) {
-                score++
-            }
-            currentQuiz++
-            if (currentQuiz < quizData.length) {
-                loadQuiz()
-            } else {
-                quiz.innerHTML = `
-           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-           <button onclick="location.reload()">Reload</button>
-           `
-            }
-        }
+function displayScore(score, name) {
+document.getElementById("question_area").classList.add("invisible");
+document.getElementById("score").classList.remove("hidden");
+let myScore = document.getElementById("correct");
+let myInncorect = document.getElementById("incorrect");
+let userName = document.getElementById("player_name");
+userName.innerText=name;
+myInncorect.innerText = 4 - score;
+myScore.innerText = score;
+}
 
-    })
+function restartGame() {
+score = 0;
+currentQuiz = 0;
+playerName = '';
+cleareInput();
+document.getElementById("welcome").classList.remove("invisible");
+document.getElementById("score").classList.add("hidden");
+document.getElementById("bio_next_submit").classList.remove("invisible");
+document.getElementById("geo_next_submit").classList.remove("invisible");
 
+}
+
+function cleareInput() {
+document.getElementById('myInput').value = ''; // Set the value to an empty string
 }
